c152446f07b68dfb4491bdbb7542a6fceccc4754
jatin0670 <jp0670786@gmail.com>
2026-05-13 16:05:33 +0530

    files

--- src/Components/ToolCards.jsx
+++ src/Components/ToolCards.jsx
@@ -7,7 +7,7 @@ const ToolCards = ({title, dis, to}) => {
       <h1 className='text-gray-800 text-lg md:text-2xl lg:text-3xl font-black'>{title}</h1>
       <p className=' text-gray-800 text-xs md:text-lg text-center'>{dis}</p>
       <Link to={to} target="_blank" rel="noreferrer">
-      <button className=' px-5 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 bg-gray-500 text-white text-xl rounded-lg font-bold cursor-pointer'>ClicK</button>
+      <button className=' px-5 py-1 md:px-6 md:py-2 lg:px-8 lg:py-2 bg-gray-500 text-white text-xl rounded-lg font-bold cursor-pointer'>Click</button>
       </Link>
     </div>
   )
